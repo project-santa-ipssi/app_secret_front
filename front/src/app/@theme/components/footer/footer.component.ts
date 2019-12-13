7441abd1d5aa162a7179235cb8d5901160219891
nhannafi <nour.hannafi@engie.com>
2019-12-13 14:21:32 +0100

    update footer

--- front/src/app/@theme/components/footer/footer.component.ts
+++ front/src/app/@theme/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import {} from '@nebular/theme';
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
-  template: ` <p> Secret Santa 2019 -   © Copyright     IPSSI dévelopeurs:   Nour HANNAFI   ,   Allan  ,   BENJAMIN  ,   Abdoulaye TRAOEe  </p>`,
+  template: ` <p> Secret Santa 2019 -   © Copyright     IPSSI dévelopeurs:   Nour HANNAFI   ,   Allan JAEGLY  ,   BENJAMIN  ,   Abdoulaye TRAORE  </p>`,
 })
 export class FooterComponent {
 }
